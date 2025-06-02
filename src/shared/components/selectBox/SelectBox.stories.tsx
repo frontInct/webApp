9ebe96863b6a303f0ee4ad262736f8130d784ba0
Davydovich <kostiarabota@gmail.com>
2025-06-02 13:50:32 +0300

    feat(IT-8): add SelectBox component and stories

--- src/shared/components/selectBox/SelectBox.stories.tsx
+++ src/shared/components/selectBox/SelectBox.stories.tsx
@@ -1,50 +1,64 @@
-// Selector.stories.tsx
 import type { Meta, StoryObj } from '@storybook/react'
-import { Selector } from './SelectBox'
+import { SelectBox, SelectBoxOption } from './SelectBox'
 
-const meta: Meta<typeof Selector> = {
-  title: 'Components/Selector',
-  component: Selector,
-  tags: ['autodocs'],
-  argTypes: {
-    onChange: { action: 'selected' },
-    disabled: { control: 'boolean' },
+const meta: Meta<typeof SelectBox> = {
+  title: 'Components/SelectBox',
+  component: SelectBox,
+  parameters: {
+    backgrounds: {
+      default: 'dark',
+      values: [{ name: 'dark', value: '#121112' }],
+    },
   },
+  tags: ['autodocs'],
 }
 
 export default meta
-type Story = StoryObj<typeof Selector>
+type Story = StoryObj<typeof SelectBox>
 
+const mockOptions: SelectBoxOption[] = [
+  { value: '1', label: 'Вариант 1' },
+  { value: '2', label: 'Вариант 2' },
+  { value: '3', label: 'Вариант 3' },
+]
 
+// Базовая история
 export const Default: Story = {
   args: {
-    options: [
-      { value: 'option1', label: 'Вариант 1' },
-      { value: 'option2', label: 'Вариант 2' },
-      { value: 'option3', label: 'Вариант 3' },
-    ],
+    options: mockOptions,
     placeholder: 'Выберите вариант',
   },
 }
 
-
+// С выбранным значением по умолчанию
 export const WithDefaultValue: Story = {
   args: {
-    ...Default.args,
-    defaultValue: 'option2',
+    options: mockOptions,
+    defaultValue: '3',
+    placeholder: 'Выберите вариант',
   },
 }
 
-
+// Заблокированный селект
 export const Disabled: Story = {
   args: {
-    ...Default.args,
+    options: mockOptions,
+    defaultValue: '2',
     disabled: true,
   },
 }
 
+// С кастомным заголовком
+export const WithTitle: Story = {
+  args: {
+    options: mockOptions,
+    title: 'Выберите вариант:',
+    placeholder: 'Выберите вариант',
+  },
+}
 
-export const Empty: Story = {
+// Пустой список
+export const EmptyOptions: Story = {
   args: {
     options: [],
     placeholder: 'Нет доступных вариантов',
--- storybook-static/sb-addons/storybook-core-core-server-presets-0/common-manager-bundle.js
+++ storybook-static/sb-addons/storybook-core-core-server-presets-0/common-manager-bundle.js
@@ -1,3 +1,71 @@
-try{
-(()=>{var v=__STORYBOOK_API__,{ActiveTabs:A,Consumer:k,ManagerContext:O,Provider:T,RequestResponseError:h,addons:l,combineParameters:g,controlOrMetaKey:U,controlOrMetaSymbol:f,eventMatchesShortcut:D,eventToShortcut:x,experimental_MockUniversalStore:P,experimental_UniversalStore:M,experimental_requestResponse:R,experimental_useUniversalStore:C,isMacLike:B,isShortcutTaken:E,keyToSymbol:I,merge:K,mockChannel:N,optionOrAltSymbol:G,shortcutMatchesShortcut:L,shortcutToHumanString:Y,types:q,useAddonState:F,useArgTypes:H,useArgs:j,useChannel:V,useGlobalTypes:z,useGlobals:J,useParameter:Q,useSharedState:W,useStoryPrepared:X,useStorybookApi:Z,useStorybookState:$}=__STORYBOOK_API__;var a=(()=>{let e;return typeof window<"u"?e=window:typeof globalThis<"u"?e=globalThis:typeof window<"u"?e=window:typeof self<"u"?e=self:e={},e})(),m="tag-filters",_="static-filter";l.register(m,e=>{let u=Object.entries(a.TAGS_OPTIONS??{}).reduce((o,s)=>{let[r,p]=s;return p.excludeFromSidebar&&(o[r]=!0),o},{});e.experimental_setFilter(_,o=>{let s=o.tags??[];return(s.includes("dev")||o.type==="docs")&&s.filter(r=>u[r]).length===0})});})();
-}catch(e){ console.error("[Storybook] One of your manager-entries failed: " + import.meta.url, e); }
+try {
+  ;(() => {
+    var v = __STORYBOOK_API__,
+      {
+        ActiveTabs: A,
+        Consumer: k,
+        ManagerContext: O,
+        Provider: T,
+        RequestResponseError: h,
+        addons: l,
+        combineParameters: g,
+        controlOrMetaKey: U,
+        controlOrMetaSymbol: f,
+        eventMatchesShortcut: D,
+        eventToShortcut: x,
+        experimental_MockUniversalStore: P,
+        experimental_UniversalStore: M,
+        experimental_requestResponse: R,
+        experimental_useUniversalStore: C,
+        isMacLike: B,
+        isShortcutTaken: E,
+        keyToSymbol: I,
+        merge: K,
+        mockChannel: N,
+        optionOrAltSymbol: G,
+        shortcutMatchesShortcut: L,
+        shortcutToHumanString: Y,
+        types: q,
+        useAddonState: F,
+        useArgTypes: H,
+        useArgs: j,
+        useChannel: V,
+        useGlobalTypes: z,
+        useGlobals: J,
+        useParameter: Q,
+        useSharedState: W,
+        useStoryPrepared: X,
+        useStorybookApi: Z,
+        useStorybookState: $,
+      } = __STORYBOOK_API__
+    var a = (() => {
+        let e
+        return (
+          typeof window < 'u'
+            ? (e = window)
+            : typeof globalThis < 'u'
+              ? (e = globalThis)
+              : typeof window < 'u'
+                ? (e = window)
+                : typeof self < 'u'
+                  ? (e = self)
+                  : (e = {}),
+          e
+        )
+      })(),
+      m = 'tag-filters',
+      _ = 'static-filter'
+    l.register(m, e => {
+      let u = Object.entries(a.TAGS_OPTIONS ?? {}).reduce((o, s) => {
+        let [r, p] = s
+        return p.excludeFromSidebar && (o[r] = !0), o
+      }, {})
+      e.experimental_setFilter(_, o => {
+        let s = o.tags ?? []
+        return (s.includes('dev') || o.type === 'docs') && s.filter(r => u[r]).length === 0
+      })
+    })
+  })()
+} catch (e) {
+  console.error('[Storybook] One of your manager-entries failed: ' + import.meta.url, e)
+}
