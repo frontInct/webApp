dc9762014837c336f949bafb775553372102ff4f
barkidov <zpliskin@inbox.ru>
2025-06-14 22:17:03 +0300

    feat(IT-63): implement Resend Verification Link page with styles

--- src/app/(status)/email-expired/page.tsx
+++ src/app/(status)/email-expired/page.tsx
@@ -1,17 +1,32 @@
 'use client'
+import { Header } from '@/widgets/header'
 import { useRouter } from 'next/navigation'
+import styles from './EmailExpiredPage.module.scss'
+import { Typography } from '@/shared/components/Typography'
+import { Button } from '@/shared/components/button'
+import ClockImg from '@/shared/assets/images/img-with-clock.svg'
+import { Input } from '@/shared/components/input'
 
 export default function EmailExpiredPage() {
   const router = useRouter()
   return (
-    <div>
-      <h2>Email verification link expired</h2>
-      <span>
-        Looks like the verification link has expired. <br />
-        Not to worry, we can send the link again
-      </span>
-      <br />
-      <button onClick={() => router.push('/sign-in')}>Resend verification link</button>
+    <div className={styles.container}>
+      <Header isLoggedIn />
+      <div className={styles.mainContent}>
+        <Typography
+          component='h1'
+          variant='H1'
+          className={styles.messageTitle}
+        >
+          Email verification link expired
+        </Typography>
+        <Typography className={styles.messageText}>
+          Looks like the verification link has expired. Not to worry, we can send the link again
+        </Typography>
+        <Input className={styles.messageInput} variant={'inputDefault'} label={'Email'} placeholder='Epam@epam.com'/>
+        <Button className={styles.messageButton} onClick={() => router.push('/sign-in')}>Resend verification link</Button>
+        <ClockImg className={styles.image}/>
+      </div>
     </div>
   )
 }
