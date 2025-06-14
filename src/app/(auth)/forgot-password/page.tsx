fb133ee3ce3b6af02d4c36f61214b219f6443235
DmtrNz <naz.dmtr@gmail.com>
2025-06-14 20:39:17 +0300

    feat(IT-74): add pageWrapper, authLayout

--- src/app/(auth)/forgot-password/page.tsx
+++ src/app/(auth)/forgot-password/page.tsx
@@ -1,50 +1,63 @@
 'use client'
 
 import Link from 'next/link'
-import { useRouter } from 'next/navigation'
+import { PageWrapper } from '../../../shared/components/pageWrapper/PageWrapper'
+import { AuthLayout } from '@/shared/components/authLayout'
+import { Button } from '@/shared/components/button'
+import { Typography } from '@/shared/components/Typography/Typography'
+import { Input } from '@/shared/components/input/Input'
 
-export default function ForgotPasswordPage() {
-  const router = useRouter()
 
+const Page = () => {
   return (
-    <main>
-      <h1>Забыли пароль</h1>
-      <p>Форма восстановления пароля</p>
-      <form
-        action='#'
-        method='POST'
-      >
-        <div>
-          <label htmlFor='email'>Email</label>
-          <br />
-          <input
-            type='email'
-            id='email'
-            name='email'
-            placeholder='your@email.com'
-          />
-        </div>
-
-        <div>
-          <label>
-            <input
-              type='checkbox'
-              name='not-robot'
+    <PageWrapper>
+      <AuthLayout>
+        <Typography component="h1" variant="H1">
+          Forgot Password
+        </Typography>
+        <form style={{ marginTop: "35px" }}
+          action='#'
+          method='POST'
+        >
+          <div>
+            <Input
+              variant="inputDefault"
+              type="email"
+              id="email"
+              name="email"
+              label="Email"
+              placeholder="Epam@epam.com"
             />
-            <span>I&#39;m not a robot</span>
-          </label>
-        </div>
-        <div>
-          <button
-            type='button'
-            onClick={() => router.push('/sign-in')}
-          >
-            Send Link
-          </button>
-          <br />
-          <Link href='/sign-in'>Back to Sign In</Link>
-        </div>
-      </form>
-    </main>
+          </div>
+          <Typography component="p" variant="regular_text_14" style={{ marginTop: '10px', marginBottom: "15px", color: "var(--color-primary-light-900)" }}>
+            Enter your email address and we will send you further instructions
+          </Typography>
+          <div>
+          </div>
+          <div style={{ display: 'flex', gap: '16px', flexDirection: 'column', marginTop: '15px' }}>
+            <Button variant="primary">
+              Send Link
+            </Button>
+            <Button variant="text" asChild>
+              <Link href="/sign-in">
+                Back to Sign In
+              </Link>
+            </Button>
+            <div>
+                <label>
+              <input
+                type='checkbox'
+                name='not-robot'
+              />
+            </label>
+            <Typography component="span" variant="regular_text_14" style={{ marginLeft: '10px' }}> 
+              I'm not a robot (заглушка)
+            </Typography>
+            </div>
+          </div>
+        </form>
+      </AuthLayout>
+    </PageWrapper>
   )
 }
+export default Page
