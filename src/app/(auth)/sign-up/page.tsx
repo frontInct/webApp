41dcfda097cd90d28b617b7b4a1158225332982b
Timur Ibragimov <bazretdinovich@gmail.com>
2025-06-15 17:50:44 +0500

    feat(IT-58): is completed

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -2,13 +2,20 @@
 import { PageWrapper } from '@/shared/components/pageWrapper'
 import { AuthLayout } from '@/shared/components/authLayout'
 import SignUpForm from '@/features/auth/signUp/SignUpForm'
-import s from '@/app/(auth)/sign-in/SignInPage.module.scss'
+import s from '@/app/(auth)/sign-up/SignUp.module.scss'
+import { Typography } from '@/shared/components/Typography'
+import GoogleIcon from '@/shared/assets/icons/google-icon.svg'
+import GitHubIcon from '@/shared/assets/icons/github-icon.svg'
 
 export default function SignUpPage() {
   return (
     <main className={s.main}>
     <PageWrapper>
       <AuthLayout>
+        <Typography variant='H1' className={s.title}>Sign Up</Typography>
+        <div className={s.svgContainer}>
+          {<GoogleIcon />} {<GitHubIcon />}
+        </div>
         <SignUpForm/>
       </AuthLayout>
     </PageWrapper>
