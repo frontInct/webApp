cb82180e31e031ab436a7218d3f895adde49575a
DmtrNz <naz.dmtr@gmail.com>
2025-06-19 20:37:58 +0300

    feat(IT-71): create password-recovery page

---
+++ src/app/(auth)/password-recovery/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { PasswordRecovery } from "@/features/auth/password/password-recovery"
+import { PageWrapper } from "@/shared/components/pageWrapper"
+
+const Page = () => {
+  return (
+    <PageWrapper>
+      <PasswordRecovery/>
+    </PageWrapper>
+  )
+}
+
+export default Page
